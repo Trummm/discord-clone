566627257c5b82b28bb7814478fbf28b1efa6d36
Trummm <trungpro2000.tp2@gmail.com>
2023-09-03 12:55:23 +0700

    invite friends

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -18,6 +18,7 @@ import {
   UserPlus,
   Users,
 } from 'lucide-react'
+import { useModal } from '@/hooks/use-modal-store'
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles
@@ -25,6 +26,8 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal()
+
   const isAdmin = role === MemberRole.ADMIN
   const isModerator = isAdmin || role === MemberRole.MONERATOR
 
@@ -39,7 +42,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
 
       <DropdownMenuContent className='w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]'>
         {isModerator && (
-          <DropdownMenuItem className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            onClick={() => onOpen('invite', { server })}
+            className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer'
+          >
             Invite People
             <UserPlus className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
