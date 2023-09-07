b88e84322e2b1f7f15769a35cef0554ae8032ada
Trummm <trungpro2000.tp2@gmail.com>
2023-09-07 15:45:05 +0700

    chat message, realtime chat and edit, delete mess

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -7,6 +7,7 @@ import { currentProfile } from '@/lib/current-profile'
 import { db } from '@/lib/db'
 import { ChatHeader } from '@/components/chat/chat-header'
 import { ChatInput } from '@/components/chat/chat-input'
+import { ChatMessages } from '@/components/chat/chat-messages'
 
 interface ChannelIdPageProps {
   params: {
@@ -46,7 +47,20 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         serverId={channel.serverId}
         type='channel'
       />
-      <div className='flex-1'>Future Messages</div>
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type='channel'
+        apiUrl='/api/messages'
+        socketUrl='/api/socket/messages'
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey='channelId'
+        paramValue={channel.id}
+      />
       <ChatInput
         name={channel.name}
         type='channel'
