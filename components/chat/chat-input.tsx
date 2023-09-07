ecc5e7896c72f68ee19286567cd6f49cc39138c5
Trummm <trungpro2000.tp2@gmail.com>
2023-09-07 10:43:11 +0700

    emoji bar

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -7,8 +7,10 @@ import qs from 'query-string'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Form, FormControl, FormField, FormItem } from '../ui/form'
 import { Input } from '../ui/input'
-import { Plus, Smile } from 'lucide-react'
+import { Plus } from 'lucide-react'
 import { useModal } from '@/hooks/use-modal-store'
+import { EmojiPicker } from '../emoji-picker'
+import { useRouter } from 'next/navigation'
 
 interface ChatInputProps {
   apiUrl: string
@@ -23,6 +25,7 @@ const formSchema = z.object({
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
   const { onOpen } = useModal()
+  const router = useRouter()
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -41,6 +44,8 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
       })
 
       await axios.post(url, values)
+      form.reset()
+      router.refresh()
     } catch (err) {
       console.log(err)
     }
@@ -64,15 +69,19 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     <Plus className='text-white dark:text-[#313338]' />
                   </button>
                   <Input
+                    disabled={isLoading}
+                    className='px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200'
                     placeholder={`Message ${
                       type === 'conversation' ? name : '#' + name
                     }`}
                     {...field}
-                    disabled={isLoading}
-                    className='px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200'
                   />
-                  <div className='absolute top-7 right-0'>
-                    <Smile />
+                  <div className='absolute top-7 right-8'>
+                    <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value} ${emoji}`)
+                      }
+                    />
                   </div>
                 </div>
               </FormControl>
